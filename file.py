d059905d0c1159c97ffeebf75dd1c91c8a4b5aa8
ROHEETxRIJAN <roheetboss@gmail.com>
2024-10-06 11:36:17 +0545

    Update file.py

--- file.py
+++ file.py
@@ -1,13 +1,1 @@
-import os, platform, time, sys
-os.system('xdg-open https://chat.whatsapp.com/GKCh3wB9KoU9oDuy0MsZ7v')
-try:
- import requests
-except:os.system("pip install requests")
-print('\033[1;37m\u001b[31m>>\033[0m\033[1;37m CHECKING FOR UPDATEz...')
-os.system('git pull --quiet ')
-bit = platform.architecture()[0]
-if bit == "64bit":
- print('• \033[1;37m\u001b[31m>>\033[0m\033[1;37m 64BIT FOUND')
- time.sleep(2)
- import run
- 
+import run
