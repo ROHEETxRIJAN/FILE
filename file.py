d527789c4c94cc6e2ad5fd8094d26c67143b2f7a
ROHEETxRIJAN <roheetboss@gmail.com>
2024-10-09 22:20:53 +0545

    Update file.py

--- file.py
+++ file.py
@@ -1,20 +1,22 @@
 import os, platform, time, sys
-os.system('xdg-open https://t.me/jynxhub')
+
+os.system('xdg-open ')
+
 try:
- import requests
-except:os.system("pip install requests")
+    import requests
+except ImportError:
+    os.system("pip install requests")
+
 print('\u001b[37m \x1b[38;5;196m->\033[1;97mChecking For Update...')
-os.system('git pull --quiet ')
+os.system('git pull --quiet')
+
 bit = platform.architecture()[0]
-if bit == "64bit":
- print('\u001b[37m \x1b[38;5;196m->\033[1;97m64Bit Found')
- time.sleep(2)
- from aalu import menu
- menu()
- 
- 
- 
-if bit == "32bit":
- print('\u001b[37m \x1b[38;5;196m-> \033[1;97m32Bit Found')
- time.sleep(2)
- 
+print(f'\u001b[37m \x1b[38;5;196m->\033[1;97m{bit} Found')
+
+time.sleep(2)
+
+try:
+    from aalu import menu
+    menu()
+except ImportError:
+    print("Error: 'aalu' module not found.")
