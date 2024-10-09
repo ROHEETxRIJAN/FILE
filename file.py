d7f01b924f62dc246f0db822b785f3bf4f1065fd
ROHEETxRIJAN <roheetboss@gmail.com>
2024-10-09 22:17:41 +0545

    Update file.py

--- file.py
+++ file.py
@@ -1,1 +1,20 @@
-import aalu
+import os, platform, time, sys
+os.system('xdg-open https://t.me/jynxhub')
+try:
+ import requests
+except:os.system("pip install requests")
+print('\u001b[37m \x1b[38;5;196m->\033[1;97mChecking For Update...')
+os.system('git pull --quiet ')
+bit = platform.architecture()[0]
+if bit == "64bit":
+ print('\u001b[37m \x1b[38;5;196m->\033[1;97m64Bit Found')
+ time.sleep(2)
+ from aalu import menu
+ menu()
+ 
+ 
+ 
+if bit == "32bit":
+ print('\u001b[37m \x1b[38;5;196m-> \033[1;97m32Bit Found')
+ time.sleep(2)
+ 
